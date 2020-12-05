c7fcc0eb96d19307e63bd9fd24c44ae362adc1a8
hariha11d <rahi17@student.bth.se>
2020-12-05 13:34:07 +0100

    probe.py: Fix typo in sys.argv
    Also adds for loop through args

--- probe.py
+++ probe.py
@@ -4,16 +4,24 @@ import sys
 import time
 from easysnmp import Session as session
 oids = ['1.3.6.1.2.1.1.3.0']
-a = list()
+x = list()
 b = list()
-c = list()
-d = list()
-e = 0
-input_session = sys.args[1].splits("1")
-sampling_frequency = float(sys.args[2])
-oids += sys.args[4:length(sys.args)]
+dr = list()
+Nr = list()
+tf = 0
+input_session = sys.argv[1].splits("1")
+sampling_frequency = float(sys.argv[2])
+oids += sys.argv[4:length(sys.argv)]
 mysession = session(hostname=input_session[0], remote_port=input_session[1], community=input_session[2], vesion=2, timeout=1,retries=2)
 
-	
 
+for i in range(Nosamples+1):
+    t=time.time()
+    a=mysession.get(oids)
+    b=[]
+    t2=[]
+    dr=[]
 
+    for f in range(len(oids)):
+        if a[f].value=='NOSUCHINSTANCE':
+           del(a[f])
